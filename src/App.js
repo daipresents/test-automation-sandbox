d838605bb97fee9af32eef76a1356644d2a5daca
MT <mtwork033@gmail.com>
2021-12-16 21:27:07 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          {/* Learn React */}
           Let's Start!!
         </button>
       </header>
