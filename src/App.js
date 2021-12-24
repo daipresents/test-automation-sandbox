9769a3ff333a2c08dc3df00501e90982b35c0447
daipresents <daipresents@gmail.com>
2021-12-24 22:20:30 +0900

    add header

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Let's Start!!
-        </button>
+        <h1>テスト自動化トレーニングサイトへようこそ！</h1>
       </header>
     </div>
   );
