1049fbfd722dec3c89e167a07f1c27b682fe65aa
daipresents <daipresents@gmail.com>
2021-12-24 22:47:34 +0900

    add login form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>テスト自動化トレーニングサイトへようこそ！</h1>
+        <form class="form">
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button type="submit" id="login">Login</button>
+        </form>
       </header>
     </div>
   );
