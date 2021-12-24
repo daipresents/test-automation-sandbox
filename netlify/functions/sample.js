9879e83b17fb7bed032bbb45470fad6657254f9a
daipresents <daipresents@gmail.com>
2021-12-25 01:52:20 +0900

    debugging

--- netlify/functions/sample.js
+++ netlify/functions/sample.js
@@ -1,7 +1,7 @@
 exports.handler = async (event, context) => {
   const sdk = require('api')('@mabldocs/v1.0#1wbh2zkr2mb6cs');
 
-  sdk.auth('key', 'T31lCOE52cG2WjOa5LcAfA')
+  sdk.auth('key', 'HFZS3u6Ri3clMwr4TH8w3w')
   sdk.post('/events/deployment', {
     plan_overrides: {
       http_headers: [{name: 'Content-Type', value: 'application/json', log_header_value: true}],
