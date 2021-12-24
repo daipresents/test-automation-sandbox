ba48c64862c338e33e0666ebe1460774f260c05f
daipresents <daipresents@gmail.com>
2021-12-25 02:26:33 +0900

    debug

--- netlify/functions/hello/main.go
+++ netlify/functions/hello/main.go
@@ -1,39 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"strings"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"net/http"
-	"io/ioutil"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-
-	url := "https://api.mabl.com/events/deployment"
-
-	payload := strings.NewReader("{\"plan_overrides\":{\"http_headers\":[{\"name\":\"Content-Type\",\"value\":\"application/json\",\"log_header_value\":true}],\"actions\":{\"rebaseline_images\":true,\"set_static_baseline\":true},\"properties\":{\"repository_url\":\"git@github.com:daipresents/test-automation-sandbox.git\"},\"uri\":\"https://create-top-page--test-automation-sandbox.netlify.app/\",\"credentials_required\":false,\"http_auth_credentials_required\":false,\"revision\":\"6da4dde\"},\"environment_id\":\"bIIfJiJ4lefJcKozgmNBPw-e\",\"application_id\":\"Kew3dKp0yhv4MDOPXGNu7w-a\"}")
-
-	req, _ := http.NewRequest("POST", url, payload)
-
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Authorization", "Basic a2V5OkhGWlMzdTZSaTNjbE13cjRUSDh3M3c=")
-
-	res, _ := http.DefaultClient.Do(req)
-
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-
-	fmt.Println(res)
-	fmt.Println(string(body))
-
 	return &events.APIGatewayProxyResponse{
 		StatusCode:        200,
 		Headers:           map[string]string{"Content-Type": "text/plain"},
-		Body:              "Called Development Event API.",
+		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
+		Body:              "Hello, World!",
+		IsBase64Encoded:   false,
 	}, nil
 }
 
 func main() {
+	// Make the handler available for Remote Procedure Call
 	lambda.Start(handler)
 }
