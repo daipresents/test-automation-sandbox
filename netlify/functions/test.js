e96e44d55e8d99983dea515733be43227e01835f
daipresents <daipresents@gmail.com>
2021-12-25 01:22:48 +0900

    change format

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,9 +1,9 @@
 exports.handler = async (event, context) => {
   const sdk = require('api')('@mabldocs/v1.0#1wbh2zkr2mb6cs');
   
-  console.log(process.env.COMMIT_REF)
-  
-  sdk.auth('key', process.env.MABL_API_KEY)
+  console.log(COMMIT_REF)
+
+  sdk.auth('key', MABL_API_KEY)
   sdk.post('/events/deployment', {
     plan_overrides: {
       http_headers: [{name: 'string', value: 'string', log_header_value: true}],
@@ -14,7 +14,7 @@ exports.handler = async (event, context) => {
       credentials_required: true,
       http_auth_credentials_id: 'string',
       http_auth_credentials_required: true,
-      revision: process.env.COMMIT_REF
+      revision: COMMIT_REF
     },
     environment_id: 'bIIfJiJ4lefJcKozgmNBPw-e',
     application_id: 'Kew3dKp0yhv4MDOPXGNu7w-a'
