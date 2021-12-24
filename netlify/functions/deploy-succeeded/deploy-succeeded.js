55403b6fbf33f6ceb9c17bb07472af875ab71a53
daipresents <daipresents@gmail.com>
2021-12-24 23:30:48 +0900

    add netlify setting for functions

---
+++ netlify/functions/deploy-succeeded/deploy-succeeded.js
@@ -0,0 +1,34 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const handler = async (event) => {
+  try {
+    //const subject = event.queryStringParameters.name || 'World'
+
+    let response
+    try {
+      response = await fetch('https://api.mabl.com/events/deployment', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ environment_id:bIIfJiJ4lefJcKozgmNBPw-e, application_id: Kew3dKp0yhv4MDOPXGNu7w-a})
+      })
+    } catch (err) {
+      return {
+        statusCode: err.statusCode || 500,
+        body: JSON.stringify({
+          error: err.message
+        })
+    }
+
+    return {
+
+      statusCode: 200,
+      //body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify({ message: `Called Deployment Event API.` }),
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { handler }
