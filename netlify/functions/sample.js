58872a18267ee3a7d827fd31bbd1a2f560701659
daipresents <daipresents@gmail.com>
2021-12-25 01:49:22 +0900

    debugging

--- netlify/functions/sample.js
+++ netlify/functions/sample.js
@@ -1,22 +1,20 @@
 exports.handler = async (event, context) => {
   const sdk = require('api')('@mabldocs/v1.0#1wbh2zkr2mb6cs');
 
-  sdk.auth('key', process.env.MABL_API_KEY)
+  sdk.auth('key', 'T31lCOE52cG2WjOa5LcAfA')
   sdk.post('/events/deployment', {
     plan_overrides: {
-      http_headers: [{name: 'string', value: 'string', log_header_value: true}],
+      http_headers: [{name: 'Content-Type', value: 'application/json', log_header_value: true}],
       actions: {rebaseline_images: true, set_static_baseline: true},
-      plan_labels: ['string'],
-      uri: 'string',
-      credentials_id: 'string',
-      credentials_required: true,
-      http_auth_credentials_id: 'string',
-      http_auth_credentials_required: true,
-      //revision: process.env.COMMIT_REF
+      properties: {repository_url: 'git@github.com:daipresents/test-automation-sandbox.git'},
+      uri: 'https://create-top-page--test-automation-sandbox.netlify.app/',
+      credentials_required: false,
+      http_auth_credentials_required: false,
+      revision: '6da4dde'
     },
     environment_id: 'bIIfJiJ4lefJcKozgmNBPw-e',
     application_id: 'Kew3dKp0yhv4MDOPXGNu7w-a'
   })
     .then(res => console.log(res))
-    .catch(err => console.error(err));
+    .catch(err => console.error(err));  
 }
