d16a1d456fd3dae36c4d180783bc03e407baf8c6
daipresents <daipresents@gmail.com>
2021-12-24 23:52:51 +0900

    add environment variable for API key

--- netlify/functions/deploy-succeeded/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded/deploy-succeeded.js
@@ -8,7 +8,8 @@ const handler = async (event) => {
       response = await fetch('https://api.mabl.com/events/deployment', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          'x-api-key': process.env.MABL_API_KEY,
         },
         body: JSON.stringify({ environment_id:bIIfJiJ4lefJcKozgmNBPw-e, application_id: Kew3dKp0yhv4MDOPXGNu7w-a})
       })
