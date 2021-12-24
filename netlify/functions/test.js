8f7e90b05fe76b5a29ad4ba4416d0da7d33fcf44
daipresents <daipresents@gmail.com>
2021-12-25 00:59:49 +0900

    add test.js

---
+++ netlify/functions/test.js
@@ -0,0 +1,22 @@
+exports.handler = async (event, context) => {
+  const sdk = require('api')('@mabldocs/v1.0#1wbh2zkr2mb6cs');
+
+  sdk.auth('key', process.env.MABL_API_KEY)
+  sdk.post('/events/deployment', {
+    plan_overrides: {
+      http_headers: [{name: 'string', value: 'string', log_header_value: true}],
+      actions: {rebaseline_images: true, set_static_baseline: true},
+      plan_labels: ['string'],
+      uri: 'string',
+      credentials_id: 'string',
+      credentials_required: true,
+      http_auth_credentials_id: 'string',
+      http_auth_credentials_required: true,
+      revision: process.env.COMMIT_REF
+    },
+    environment_id: 'bIIfJiJ4lefJcKozgmNBPw-e',
+    application_id: 'Kew3dKp0yhv4MDOPXGNu7w-a'
+  })
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+}
