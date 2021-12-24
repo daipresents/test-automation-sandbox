302064ed5e884e423f081d9f7c8123fc94d660da
daipresents <daipresents@gmail.com>
2021-12-25 02:01:13 +0900

    debugging

--- netlify/functions/sample.js
+++ netlify/functions/sample.js
@@ -1,6 +1,8 @@
 exports.handler = async (event, context) => {
   const sdk = require('api')('@mabldocs/v1.0#1wbh2zkr2mb6cs');
 
+  console.log(process.env.NODE_ENV)
+
   sdk.auth('key', 'HFZS3u6Ri3clMwr4TH8w3w')
   sdk.post('/events/deployment', {
     plan_overrides: {
@@ -16,5 +18,5 @@ exports.handler = async (event, context) => {
     application_id: 'Kew3dKp0yhv4MDOPXGNu7w-a'
   })
     .then(res => console.log(res))
-    .catch(err => console.error(err));  
+    .catch(err => console.error(err));
 }
