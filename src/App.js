e078bf1f6cef1baffd1ccb1dbf4f9d22aca64cd1
daipresents <daipresents@gmail.com>
2021-12-28 00:18:43 +0900

    create top page.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './pages/Home';
+import TopMenu from './pages/TopMenu';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>テスト自動化トレーニングサイトへようこそ！</h1>
-        <form class="form">
-          <input type="text" placeholder="メールアドレス" />
-          <input type="password" placeholder="パスワード" />
-          <button type="submit" id="login">ログイン</button>
-        </form>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="top-menu" element={<TopMenu />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
