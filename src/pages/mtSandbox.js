7288f2b8566091553c220d56e77fdd70dddefdc7
MT <mtwork033@gmail.com>
2022-01-20 16:55:43 +0900

    remove contents

--- src/pages/mtSandbox.js
+++ src/pages/mtSandbox.js
@@ -2,21 +2,10 @@ import {
     Link
   } from "react-router-dom";
   
-  export default function TopMenu() {
+  export default function mtSandbox() {
     return (
       <div>
-        <h1>メニュー</h1>
-        <h2>お品書き</h2>
-        <div>
-          ブラウザE2Eレベルのテスト自動化のポイントをまとめています。
-        </div>
-        <ul>
-          <li>WaitUntil： ページ読み込みタイミングをコントロールする</li>
-          <li>Table： たくさんあるものから「ひとつ」を選ぶ</li>
-        </ul>
-        <div>
-          <Link to="/">ログアウト</Link>
-        </div>
+        <h1>砂場</h1>
       </div>
     );
   }
